c600f2a409ca72ac7208d3c27e670a4b95f7b861
Mika21r <mih.kirill0318@gmail.com>
2024-12-06 14:42:40 +0300

    check5 commit

--- index.js
+++ index.js
@@ -2,20 +2,24 @@
 const toggleBtn = document.querySelector('.toggle');
 const nav = document.querySelector('.nav');
 const navList = document.querySelector('.nav-list');
-const navItems = document.querySelectorAll('.nav-item');
+const navContainer = document.querySelectorAll('.nav-container');
 
 function switchMenu() {
-  toggleBtn.classList.toggle('collapsed');
-  nav.classList.toggle('collapsed');
-  navList.classList.toggle('collapsed');
+  if (toggleBtn) toggleBtn.classList.toggle('collapsed');
+  if (nav) nav.classList.toggle('collapsed');
+  if (navList) navList.classList.toggle('collapsed');
 }
-toggleBtn.addEventListener('click', switchMenu);
-
-function closeMenu() {
-  setTimeout(() => {
-    toggleBtn.classList.remove('collapsed');
-    nav.classList.remove('collapsed');
-    navList.classList.remove('collapsed');
-  }, 1000);  
-}
-navItems.forEach(el => el.addEventListener('click', closeMenu));
+if (toggleBtn) toggleBtn.addEventListener('click', switchMenu);
+if (navContainer) navContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('nav-item')) {
+    switchMenu();
+  }
+});
+// function closeMenu() {
+//   setTimeout(() => {
+//     toggleBtn.classList.remove('collapsed');
+//     nav.classList.remove('collapsed');
+//     navList.classList.remove('collapsed');
+//   }, 1000);  
+// }
+// navItems.forEach(el => el.addEventListener('click', closeMenu));
